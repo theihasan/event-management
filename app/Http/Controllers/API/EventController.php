e99e106ad7973ba7e6af408b9f7be087f7a47c91
Md Abul Hassan <imabulhasan99@gmail.com>
2024-07-05 10:39:44 +0600

    add authorization for event oparation

--- app/Http/Controllers/API/EventController.php
+++ app/Http/Controllers/API/EventController.php
@@ -9,16 +9,19 @@ use App\Http\Resources\EventResource;
 use App\Models\Event;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class EventController extends Controller
 {
     public function index()
     {
+        Gate::authorize('viewAny', Event::class);
         return EventResource::collection(Event::paginate(10));
     }
 
     public function store(EventStoreRequest $request)
     {
+        Gate::authorize('create', Event::class);
         $eventData = $request->validated();
         $eventData['user_id'] = auth()->id();
         Event::create($eventData);
@@ -29,11 +32,13 @@ class EventController extends Controller
 
     public function show(Event $event): EventResource
     {
+        Gate::authorize('view', $event);
         return EventResource::make($event);
     }
 
     public function update(EventUpdateRequest $request, Event $event): JsonResponse
     {
+        Gate::authorize('update', $event);
         $event->update($request->validated());
         return response()->json([
             'message' => 'Event updated successfully',
@@ -42,6 +47,7 @@ class EventController extends Controller
 
     public function destroy(Event $event): JsonResponse
     {
+        Gate::authorize('delete', $event);
         $event->delete();
         return response()->json([
             'message' => 'Event deleted successfully',
